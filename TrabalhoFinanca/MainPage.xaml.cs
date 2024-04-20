489858301893675da879e1013f0c445007e6f4cf
HallanBre <halllanbandrad@gmail.com>
2024-04-19 21:17:02 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoFinanca/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace TrabalhoFinanca
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void cliquebtn(object sender, EventArgs e)
+        {
+            string simboloAcao = campoSimbolo.Text;
+            telaCot shareDetails = new telaCot(simboloAcao);
+            telaCot telaCot = new telaCot(simboloAcao);
+            await Navigation.PushAsync(telaCot);
+            SemanticScreenReader.Announce(simboloAcao);
+        }
+    }
+
+}
